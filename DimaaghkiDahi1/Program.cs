ccd1a0e246ed8f83e52686c8295407b02a5732f2
MuhammadAwais153 <144766305+MuhammadAwais153@users.noreply.github.com>
2023-10-05 20:50:39 +0500

    Grading System
    It is a short program which i am making as an assignment.

--- DimaaghkiDahi1/Program.cs
+++ DimaaghkiDahi1/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-namespace Calculator1
+namespace Methods
 {
     internal class Program
     {
         private static int Result;
+
         public static void Add(int a, int b)
         {
             Result = a + b;
@@ -25,16 +26,88 @@ namespace Calculator1
             Result = a / b;
         }
 
+        public static void Pass(int a)
+        {
+            if (a > 35)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+
+        public static void AGrade(int a)
+        {
+            if (a >= 96)
+            {
+                Console.WriteLine("A+ Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public static void BGrade(int a)
+        {
+            if (a >= 73)
+            {
+                Console.WriteLine("B Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public static void CGrade(int a)
+        {
+            if (a >= 61)
+            {
+                Console.WriteLine("C Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public static void DGrade(int a)
+        {
+            if (a >= 54)
+            {
+                Console.WriteLine("D Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        private static void Fail(int a)
+            {
+                if (a <= 45)
+                {
+                    Console.WriteLine("F Grade");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+
         public static void Main(string[] args)
         {
-            Add(10, 45);
-            Console.WriteLine("Answer is: " + Result);
-            Subtract(50, 12);
-            Console.WriteLine("Answer is: " + Result);
-            Multiply(100, 74);
-            Console.WriteLine("Answer is: " + Result);
-            Divide(12,85);
-            Console.WriteLine("Answer is: " + Result);
+            Pass(100);
+            AGrade(100);
+            Pass(75);
+            BGrade(75);
+            Pass(64);
+            CGrade(64);
+            Pass(51);
+            DGrade(51);
+            Pass(45);
+            Fail(45);
         }
     }
 }
