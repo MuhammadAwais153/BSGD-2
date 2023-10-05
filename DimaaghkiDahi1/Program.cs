b5da42b74ac874dcade7c2b04e0ef807512bc07a
MuhammadAwais153 <144766305+MuhammadAwais153@users.noreply.github.com>
2023-10-05 22:47:49 +0500

    Dota 2 Heroes Database

--- DimaaghkiDahi1/Program.cs
+++ DimaaghkiDahi1/Program.cs
@@ -1,40 +1,110 @@
 ﻿using System;
 
-namespace Calculator1
+namespace Awais
 {
     internal class Program
     {
-        private static int Result;
-        public static void Add(int a, int b)
-        {
-            Result = a + b;
-        }
-
-        public static void Subtract(int a, int b)
-        {
-            Result = a - b;
-        }
-
-        public static void Multiply(int a, int b)
-        {
-            Result = a * b;
-        }
-
-        public static void Divide(int a, int b)
-        {
-            Result = a / b;
-        }
-
         public static void Main(string[] args)
         {
-            Add(10, 45);
-            Console.WriteLine("Answer is: " + Result);
-            Subtract(50, 12);
-            Console.WriteLine("Answer is: " + Result);
-            Multiply(100, 74);
-            Console.WriteLine("Answer is: " + Result);
-            Divide(12,85);
-            Console.WriteLine("Answer is: " + Result);
+            // Hero1
+            string name1 = "Axe";
+            int strength1 = 44;
+            float damage1 = 70.0F;
+
+            Console.WriteLine("Hero 1:");
+            Console.WriteLine("Name: " + name1);
+            Console.WriteLine("Strength: " + strength1);
+            Console.WriteLine("Damage: " + damage1);
+
+            // Hero2
+            string name2 = "Bristleback";
+            int strength2 = 50;
+            float damage2 = 78.0F;
+
+            Console.WriteLine("\nHero 2:");
+            Console.WriteLine("Name: " + name2);
+            Console.WriteLine("Strength: " + strength2);
+            Console.WriteLine("Damage: " + damage2);
+
+            // Hero3
+            string name3 = "Chaos Knight";
+            int strength3 = 48;
+            float damage3 = 74.0F;
+
+            Console.WriteLine("\nHero 3:");
+            Console.WriteLine("Name: " + name3);
+            Console.WriteLine("Strength: " + strength3);
+            Console.WriteLine("Damage: " + damage3);
+
+            // Hero
+            string name4 = "Dragon Knight";
+            int strength4 = 54;
+            float damage4 = 82.0F;
+
+            Console.WriteLine("\nHero 4:");
+            Console.WriteLine("Name: " + name4);
+            Console.WriteLine("Strength: " + strength4);
+            Console.WriteLine("Damage: " + damage4);
+
+            // Hero
+            string name5 = "Sven";
+            int strength5 = 49;
+            float damage5 = 75.0F;
+
+            Console.WriteLine("\nHero 5:");
+            Console.WriteLine("Name: " + name5);
+            Console.WriteLine("Strength: " + strength5);
+            Console.WriteLine("Damage: " + damage5);
+
+            // Hero
+            string name6 = "Sand King";
+            int strength6 = 42;
+            float damage6 = 65.0F;
+
+            Console.WriteLine("\nHero 6:");
+            Console.WriteLine("Name: " + name6);
+            Console.WriteLine("Strength: " + strength6);
+            Console.WriteLine("Damage: " + damage6);
+
+            // Hero
+            string name7 = "Pudge";
+            int strength7 = 58;
+            float damage7 = 87.0F;
+
+            Console.WriteLine("\nHero 7:");
+            Console.WriteLine("Name: " + name7);
+            Console.WriteLine("Strength: " + strength7);
+            Console.WriteLine("Damage: " + damage7);
+
+            // Hero
+            string name8 = "Tiny";
+            int strength8 = 55;
+            float damage8 = 84.0F;
+
+            Console.WriteLine("\nHero 8:");
+            Console.WriteLine("Name: " + name8);
+            Console.WriteLine("Strength: " + strength8);
+            Console.WriteLine("Damage: " + damage8);
+
+            // Hero
+            string name9 = "Nightstalker";
+            int strength9 = 60;
+            float damage9 = 90.0F;
+
+            Console.WriteLine("\nHero 9:");
+            Console.WriteLine("Name: " + name9);
+            Console.WriteLine("Strength: " + strength9);
+            Console.WriteLine("Damage: " + damage9);
+
+            // HeroN
+            string name10 = "Lycan";
+            int strength10 = 89;
+            float damage10 = 87.0F;
+
+            Console.WriteLine("\nHero 10:");
+            Console.WriteLine("Name: " + name10);
+            Console.WriteLine("Strength: " + strength10);
+            Console.WriteLine("Damage: " + damage10);
         }
     }
 }
